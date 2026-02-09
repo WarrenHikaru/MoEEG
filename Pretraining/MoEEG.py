329803e8e8e7f2a603610c318201749c2f38c7bf
Ricardo <wyx112415@163.com>
2026-02-09 14:34:40 +0800

    Update MoEEG.py

--- Pretraining/MoEEG.py
+++ Pretraining/MoEEG.py
@@ -366,7 +366,7 @@ class EEGTransformer(nn.Module):
                 self.blocks.append(
                     Block(
                         dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio,
-                        qkv_bias=qkv_bias, drop=drop_rate, attn_drop=attn_drop_rate,
+                        qkv_bias=qkv_bias,patch=self.num_patches, drop=drop_rate, attn_drop=attn_drop_rate,
                         drop_path=dpr[i], norm_layer=norm_layer,is_group_attn=True,
                         use_gate=True
                     )
@@ -483,7 +483,6 @@ class EEGTransformer(nn.Module):
         summary_token = self.summary_token.repeat((x.shape[0], 1, 1))
         x = torch.cat([x, summary_token], dim=1)  # BmN, mC+embed_num, D
 
-        # Transformer块前向传播
         for i, blk in enumerate(self.blocks):
             if isinstance(blk, MoEBlock):
                 x, aux_loss = blk(x)
